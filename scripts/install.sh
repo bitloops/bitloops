7b516e6b086b0d7b16d0d81323bf3cac3d855516
Markos Girgis <markos@bitloops.com>
2022-03-22 17:19:31 +0200

    FIx echo option for mac shell

--- scripts/install.sh
+++ scripts/install.sh
@@ -96,30 +96,14 @@ curl -X POST "http://localhost:8080/auth/admin/realms/$PID/identity-provider/ins
     "syncMode":"IMPORT"
   }
 }'
-# echo '-----BEGIN PUBLIC KEY-----' > .public_key.txt;
-# curl http://localhost:8080/auth/realms/"$PID" | jq '.public_key' | xargs echo  >> .public_key.txt;
-# echo -n '-----END PUBLIC KEY-----' >> .public_key.txt
-PK="-----BEGIN PUBLIC KEY-----"
-POSTFIX="-----END PUBLIC KEY-----"
 
-PAYLOAD=$(curl http://localhost:8080/auth/realms/$PID)# | jq '.public_key' | xargs echo  >> .public_key.txt;
-echo "Payload: $PAYLOAD"
-PK_BODY=$(jq -r '.public_key' <<<"$PAYLOAD")
-# ACESS_KEY=$( jq -r '.access_token' <<< "$RESPONSE")
+echo '-----BEGIN PUBLIC KEY-----' > .public_key.txt;
+curl http://localhost:8080/auth/realms/"$PID" | jq '.public_key' | xargs echo  >> .public_key.txt;
+echo '-----END PUBLIC KEY-----\c' >> .public_key.txt
+base64 ./.public_key.txt | xargs echo > .public_key_base64.txt
+PK=$(cat ./.public_key_base64.txt)
 
-PK+=$PK_BODY
-PK+=$POSTFIX
-
-echo "Concat PK:$PK"
-
-# base64 -w 0 ./.public_key.txt | xargs echo > .public_key_base64.txt
-# PK=$(cat ./.public_key_base64.txt)
-PK=$(base64 -w 0 $PK)
+rm .public_key.txt .public_key_base64.txt
 
 echo "Base64 Concat PK:$PK"
-# SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-# PARENT="$(dirname "$SCRIPT_DIR")"
-# sed -i.bak "s/^KEYCLOAK_PK.*$/KEYCLOAK_PK='$PK'/g" $PARENT/bitloops-rest/docker.env
-# sed -i.bak "s/^KEYCLOAK_PK.*$/KEYCLOAK_PK='$PK'/g" $PARENT/bitloops-engine/docker.env
-# echo "New PK:$PK"
 KEYCLOAK_PK=$PK docker compose up -d #--scale keycloak=0
