d62c96d4ad0e6c72be65cb87a6380474f5a9d558
frostedpenguin <baltinon2@gmail.com>
2022-03-22 16:00:37 +0200

    docker-script

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,18 +3,20 @@ STATUS_CODE=000
 docker-compose up -d keycloak
 while [ $STATUS_CODE == 000 ] 
 do 
-  STATUS_CODE=$( curl --write-out '%{http_code}\n' --silent --output /dev/null http://localhost:8080/auth/realms/master/protocol/openid-connect/token)
-  echo connecting to keycloak ... 
+  STATUS_CODE=$( curl --write-out '%{http_code}\n' --silent --output /dev/null http://localhost:8080/auth/)
   sleep 5
+  echo connecting to keycloak ... 
   done
 shopt -s lastpipe
 echo fetching token id
-curl --location --request POST 'http://localhost:8080/auth/realms/master/protocol/openid-connect/token' \
---header 'Content-Type: application/x-www-form-urlencoded' \
---data-urlencode 'grant_type=password' \
---data-urlencode 'client_id=admin-cli' \
---data-urlencode 'username=admin' \
---data-urlencode 'password=Pa55w0rd' | jq -r '.access_token' | read ACESS_KEY&&
+RESPONSE=$( curl --location --request POST 'http://localhost:8080/auth/realms/master/protocol/openid-connect/token' \
+  --header 'Content-Type: application/x-www-form-urlencoded' \
+  --data-urlencode 'grant_type=password' \
+  --data-urlencode 'client_id=admin-cli' \
+  --data-urlencode 'username=admin' \
+  --data-urlencode 'password=Pa55w0rd' ) #| jq -r '.access_token' | ACESS_KEY=$(</dev/stdin)&&
+ACESS_KEY=$( jq -r '.access_token' <<< "$RESPONSE")
+echo $ACESS_KEY
 echo insert your provider id &&
 read PID
 
