c787d14f273299a9a38076f552d470c27c9b88f1
sardounis <ant.sardounis@gmail.com>
2026-03-10 14:01:25 +0200

    added formatting on save for vscode and removed testing file

--- bitloops_cli/src/to_be_deleted_1.rs
+++
@@ -1,7 +0,0 @@
-pub fn  badly_formatted (  x:i32,y : i32)->i32{if x>y {x+y}else{ x-y }}
-
-pub struct  Demo { pub value:i32 }
-
-impl Demo{pub fn new( value:i32)->Self{Self{value}}
-    pub fn  bump(&mut self){self.value = self.value+1;}
-}
