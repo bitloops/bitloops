b3234118e069e0530d5301b00b7ec39ab56d58fe
sardounis <ant.sardounis@gmail.com>
2026-03-27 12:23:34 +0200

    Changed another comment

--- bitloops/src/main.rs
+++ bitloops/src/main.rs
@@ -19,7 +19,7 @@ pub(crate) mod test_support;
 
 #[tokio::main]
 async fn main() {
-    // comment to be deleted
+    // comment to be deleted 1
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     let cmd = cli::Cli::parse();
 
