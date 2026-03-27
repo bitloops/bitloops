6c1e8a913a8cb1ad7ad1d40586e1c240a03ae6b3
sardounis <ant.sardounis@gmail.com>
2026-03-27 10:40:12 +0200

    added test comment

--- bitloops/src/main.rs
+++ bitloops/src/main.rs
@@ -19,6 +19,7 @@ pub(crate) mod test_support;
 
 #[tokio::main]
 async fn main() {
+    // To be deleted comment
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     let cmd = cli::Cli::parse();
 
